08bde8cd4d92b4360ce25eb2b04a344386085811
Artheek18 <artheek2321.Shan@gmail.com>
2026-03-25 09:00:05 -0400

    Cleaned code and fixed formatting

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 import datetime
 
+
+st.set_page_config(page_title="NASA APOD", page_icon="🌌")
 load_dotenv()
 
 api_key = os.getenv("NASA_API_KEY")
@@ -21,17 +23,25 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={date}"
 # 🔥 Cache function
 @st.cache_data
 def get_apod(url):
-    return requests.get(url).json()
+    return requests.get(url, timeout=5).json()
 content = get_apod(url)
 
 
-st.title(content['title'])
+st.title("NASA Astronomy Picture of the Day")
+st.write("Explore NASA's Astronomy Picture of the Day.")
 st.subheader(content['date'])
+st.header(content['title'])
 
 if content["media_type"] == "image":
-    st.image(content["url"])
+    st.image(content["url"], width='stretch')
+    if content.get("hdurl"):
+        st.link_button("Open HD Image", content["hdurl"])
+
 else:
     st.video(content["url"])
+
+if content.get("copyright"):
+    st.caption(f"© {content['copyright']}")
 
 st.write(content['explanation'])
 
